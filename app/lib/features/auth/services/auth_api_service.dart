2e0a533abbedef75cb2dbd65cd50bd7bf3b6a414
Theo <theogugu06600@gmail.com>
2026-06-18 11:46:07 +0200

    Add auth api login service

--- app/lib/features/auth/services/auth_api_service.dart
+++ app/lib/features/auth/services/auth_api_service.dart
@@ -3,4 +3,22 @@ import 'package:http/http.dart' as http;
 
 class AuthApiService {
   static const String baseUrl = 'http://localhost:3000';
+
+  static Future<Map<String, dynamic>> login({
+    required String email,
+    required String password,oko
+  }) async {
+    final url = Uri.parse('$baseUrl/auth/login');
+
+    final response = await http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: jsonEncode({'email': email, 'password': password}),
+    );
+    final data = jsonDecode(response.body);
+    if (response.statusCode != 200 && response.statusCode != 201) {
+      throw Exception(data['message'] ?? 'Erreur de connexion');
+    }
+    return data;
+  }
 }
